343a303e5aba9ae1f55e6f32e507db5d2f488973
richard <richard.obiye@compulynx.com>
2018-07-20 17:33:01 +0300

    routing

---
+++ src/app/sidenavigator/sidenavigator.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-sidenavigator',
+  templateUrl: './sidenavigator.component.html',
+  styleUrls: ['./sidenavigator.component.css']
+})
+export class SidenavigatorComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  }
